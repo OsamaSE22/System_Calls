23f6b4fda3dbb3fa6c47e4d83f1872e43bbcf56b
OsamaSE22 <UsamaMahmoud2002@gmail.com>
2025-06-01 12:10:37 +0300

    added close function

---
+++ close_function.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+#include<fcntl.h> 
+#include <unistd.h>
+#include <stdlib.h>
+int main() {
+
+    // Assume that foo.txt is already created 
+    int fd1 = open("foo.txt", O_RDONLY, 0); 
+    close(fd1); 
+
+    // assume that baz.tzt is already created 
+    int fd2 = open("baz.txt", O_RDONLY, 0); 
+
+    printf("fd2 = % d\n", fd2); 
+    exit(0); 
+}
